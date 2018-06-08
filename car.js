8b7739f2a4f95aa926febee58f08eeeb7b772300
James Truitt <tru@rockyagirl.com>
2018-06-07 19:51:13 -0600

    oop 1 completed by codealong

---
+++ car.js
@@ -0,0 +1,26 @@
+class Car {
+  constructor() {
+    this.gallons = 0;
+    this.mileage = 0;
+    this.tripHistory = [];
+  }
+  fill(gallons) {
+   (this.gallons += gallons)
+  }
+  drive(miles) {
+   this.gallons = miles / 10;
+    this.mileage += miles;
+    this.tripHistory.push(`${miles} miles`);
+  }
+  odometer() {
+    return this.mileage;
+  }
+  trips() {
+    return this.tripHistory;
+  }
+
+};
+
+
+
+module.exports = Car;
---
+++ rubric.md
@@ -0,0 +1,11 @@
+# OOP
+
+## Functional
+
+* [ ] All tests pass (include an screenshot)
+* [ ] Solution uses either JavaScript class syntax or prototype-based inheritance
+
+## Style
+
+* [ ] Proper indentation
+* [ ] Consistent use of semi-colons
